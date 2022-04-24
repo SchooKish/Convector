4a4134d1aa785a89c76fea522e609e180dd2af33
SchooKish <kishkola.work@gmail.com>
2022-04-24 22:46:44 +0300

    Easy header and footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer fixed padless>
+    <v-card width="100%" class="primary text-center">
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} - <strong>SchooKish</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card class="mx-auto overflow-hidden" width="100%" height="100%">
+    <v-app-bar color="primary" dark>
+      <v-app-bar-nav-icon @click="showMenu = true" />
+
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="showMenu" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <router-link :to="{ name: 'Main' }" class="text-decoration-none">
+              <v-list-item-title>Home</v-list-item-title>
+            </router-link>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+
+  data: () => ({
+    showMenu: false,
+    group: null,
+  }),
+};
+</script>
+
+<style scoped></style>
